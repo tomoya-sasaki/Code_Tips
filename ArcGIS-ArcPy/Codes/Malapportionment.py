21746d9ac89c99e08e0e73162a597e3b0254ec20
Shusei-E <Shusei-E@users.noreply.github.com>
2016-02-25 22:29:29 +0900

    Create Malapportionment.py

---
+++ ArcGIS-ArcPy/Codes/Malapportionment.py
@@ -0,0 +1,36 @@
+import arcpy
+import re
+
+arcpy.env.workspace = r"C:/Users/admin/Desctop/14_kanagawa/14_kanagawa.gdb"
+
+in_features = "町丁字別_人口密度"
+out_feature_class = "Election_District"
+
+dissolve_field = "kanagawa$.District_code"
+
+# ある程度データを引き継ぐための準備
+fields = arcpy.ListFields(in_features)
+
+statistic_list = []
+for field in fields:
+  if re.search(r"OBJECTID", field.name):
+    continue
+  elif re.search(r"KEN", field.name):
+    statistic_list.append([field.name, "FIRST"])
+  elif re.search(r"MALE_", field.name):
+    statistic_list.append([field.name, "SUM"])
+    statistic_list.append([field.name, "FIRST"])
+  elif re.search(r"FEMALE_", field.name):
+    statistic_list.append([field.name, "SUM"])
+  elif re.search(r"POP_", field.name):
+    statistic_list.append([field.name, "SUM"])
+  elif re.search(r"X_CODE", field.name):
+    statistic_list.append([field.name, "MEAN"])
+  elif re.search(r"Y_CODE", field.name):
+    statistic_list.append([field.name, "MEAN"])
+  elif re.search(r"COORDINATE_AREA", field.name):
+    statistic_list.append([field.name, "SUM"])
+  elif re.search(r"Y_CODE", field.name):
+    statistic_list.append([field.name, "MEAN"])
+
+arcpy.Dissolve_management(in_features, out_feature_class, dissolve_field, statistic_list, "MULTI_PART", "DISSOLVE_LINES")
