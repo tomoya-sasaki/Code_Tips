45d7c34342b9e708ea823e0c2930664fbbcb612b
Shusei-E <Shusei-E@users.noreply.github.com>
2016-02-27 17:39:58 +0900

    Update Malapportionment.py

--- ArcGIS-ArcPy/Codes/Malapportionment.py
+++ ArcGIS-ArcPy/Codes/Malapportionment.py
@@ -41,10 +41,6 @@ for field in fields:
     statistic_list.append([field.name, "SUM"])
   elif re.search(r"HHW", field.name):
     statistic_list.append([field.name, "SUM"])
-  elif re.search(r"X_CODE", field.name):
-    statistic_list.append([field.name, "MEAN"])
-  elif re.search(r"Y_CODE", field.name):
-    statistic_list.append([field.name, "MEAN"])
   elif re.search(r"COORDINATE_AREA", field.name):
     statistic_list.append([field.name, "SUM"])
   elif re.search(r"Num_Votes", field.name):
