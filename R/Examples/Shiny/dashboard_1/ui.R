0b1df7711d2f345a00ede68d97971298a3794ba0
Shusei <Shusei-E@users.noreply.github.com>
2022-07-14 07:34:28 -0700

    Create ui.R

---
+++ R/Examples/Shiny/dashboard_1/ui.R
@@ -0,0 +1,49 @@
+library(shiny)
+library(shinydashboard)
+library(tidyverse)
+source("data.R")
+
+# shiny::runApp('./')
+
+dashboardPage(
+  dashboardHeader(
+    title = "Example",
+    titleWidth = 300
+  ),
+  dashboardSidebar(disable = TRUE),
+  dashboardBody(
+    fluidRow(
+    # Column 1: Parameters
+    column(width = 3,
+      box(width = NULL,
+        # Country
+        selectInput(
+          "country",
+          "Country",
+          choices = c("US", "Germany")
+        ),
+        # Proportion
+        sliderInput(
+          "prop",
+          "Proportion",
+          min = 0, max = 100, value = default_prop$US,
+          step = 10, ticks = FALSE, round = FALSE,
+          post = "%"
+        )
+      )  # close box
+    ),  # close column 1
+    column(
+      width = 8,
+      box(
+        width = NULL,
+        dataTableOutput("res_table")
+      ),
+      box(
+        width = NULL,
+        plotOutput("res_plot")
+      )
+    )
+   ) # close `fluidRow`
+  )  # close `dashboardBody`
+)
+
